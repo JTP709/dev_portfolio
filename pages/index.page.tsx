c56944f58d6e142121ff7096fdd6db1996b16c67
Jon Prell <JTP709@gmail.com>
2022-06-05 15:57:01 -0600

    add banner element to index page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Banner from './components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        {/* Banner */}
+        <Banner />
         {/* About */}
         {/* Skills */}
         {/* Experience */}
