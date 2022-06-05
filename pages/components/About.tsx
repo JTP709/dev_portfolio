2a8eb26697293877c037d5bf2a5a353ecaf3a892
Jon Prell <JTP709@gmail.com>
2022-06-05 16:18:40 -0600

    add about me info

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -1,6 +1,18 @@
 const About = () => {
     return (
-        <div data-testid='about'>About</div>
+        <div data-testid='about'>
+            <h2>About Me</h2>
+            <p>
+                Hello,there! I&apos;m Jon Prell, a senior software engineer with <a href='https//www.coterieinsurance.com'>Coterie Insurance</a>.{' '}
+                I have full-stack experience, though have been mostly focused on front-end development building React applications of all sizes{' '}
+                From early stage start-ups to Fortune 500 companies, I have written production quality code for all shapes and sizes.
+            </p>
+            <p>
+                When I&apos;m not writing code, I like to spend my time with my wonderful partner Leandra, our dog Charlie, and our demon kitty Leo.{' '}
+                After growing up in the Kentucky, we took advantage of remote work and relocated to Denver, Colorado.{' '}
+                We love to spend our week nights playing video games or watching movies, and our weekends hiking, camping, or off-roading in the Colorado mountians.
+            </p>
+        </div>
     )
 };
 
