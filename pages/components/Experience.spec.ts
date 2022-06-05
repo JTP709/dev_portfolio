7865ed487ce9c2301b690b53aca5e0e730f5e4c5
Jon Prell <JTP709@gmail.com>
2022-06-05 16:58:07 -0600

    add experience component

--- pages/components/Experience.spec.ts
+++ pages/components/Experience.spec.ts
@@ -1,4 +1,4 @@
-import Experience from "./Experience";
+import Experience, { experience_list } from "./Experience";
 import { renderComponent } from "../../utils/testUtils";
 
 const renderExperience = renderComponent(Experience);
@@ -8,5 +8,19 @@ describe('Experience', () => {
         const { getByTestId } = renderExperience();
 
         expect(getByTestId('experience')).toBeInTheDocument();
+    });
+    it.each(experience_list)('renders experience list', (experience) => {
+        const { getByTestId, getByText } = renderExperience();
+
+        expect(getByTestId(`${experience.title}-exp`)).toBeInTheDocument();
+        expect(getByText(experience.description)).toBeInTheDocument();
+
+    });
+    describe.each(experience_list)('renders', (experience) => {
+        it.each(experience.projects)('', (project) => {
+            const { getByTestId } = renderExperience();
+    
+            expect(getByTestId(`${project}-proj`)).toBeInTheDocument();
+        });
     });
 });
--- pages/components/Experience.tsx
+++ pages/components/Experience.tsx
@@ -1,6 +1,36 @@
+export const experience_list = [
+    {
+        title: 'Coterie Insurance',
+        description: 'worked for ci',
+        projects: ['Quote Flow White Labelling', 'Shared Component Library', 'Billing Center', 'Hackathon']
+    },
+    {
+        title: 'Launch Scout',
+        description: 'worked for ls',
+        projects: ['Kroger', 'GIS Dynamics', 'Coterie Insurance']
+    },
+];
+
 const Experience = () => {
     return (
-        <div data-testid='experience'>Experience</div>
+        <div data-testid='experience'>
+            <h2>Experience</h2>
+            <div>
+                {
+                    experience_list.map(experience => (
+                        <div data-testid={ `${experience.title}-exp` } key={ experience.title }>
+                            <h3>{ experience.title }</h3>
+                            <p>{ experience.description }</p>
+                            <ul>
+                                { experience.projects.map(project => (
+                                    <li data-testid={ `${project}-proj` } key={ project }>{ project }</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     );
 };
 
