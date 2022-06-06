4c8a6e8a913bd91d161f1f59216e1a31e9baf10f
Jon Prell <JTP709@gmail.com>
2022-06-05 18:41:04 -0600

    update contact component and footer

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -1,6 +1,38 @@
+export const contact_types = {
+    LINKEDIN: 'LinkedIn',
+    GITHUB: 'Github',
+    TWITTER: 'Twitter',
+};
+
+export const contact_list = [
+    {
+        type: contact_types.LINKEDIN,
+        href: 'https://www.linkedin.com/in/jon-prell-web-dev/',
+    },
+    {
+        type: contact_types.GITHUB,
+        href: 'https://github.com/JTP709/',
+    },
+    {
+        type: contact_types.TWITTER,
+        href: 'https://twitter.com/JTP709',
+    },
+];
+
 const Contact = () => {
     return (
-        <div data-testid='contact'>Contact</div>
+        <div data-testid='contact'>
+            <h2>Contact and Social</h2>
+            <ul>
+                {
+                    contact_list.map(contact => (
+                        <li key={ contact.type }>
+                            <a href={ contact.href }>{ contact.type }</a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 };
 
