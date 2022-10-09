3299ea406fc14256d9c5b5b4d3bfbed98e164b3b
Jon Prell <JTP709@gmail.com>
2022-10-09 12:06:16 -0600

    install svgr and tsParticles

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     // `.api.ts` for API routes
     'api.ts',
   ],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
